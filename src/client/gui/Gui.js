391a12d843e297a00434364582bccb9bfd3dbc8d
lepetitglacon <estebangagneur03@gmail.com>
2023-05-02 08:53:42 +0200

    [FEATURE] performance gui

--- src/client/gui/Gui.js
+++ src/client/gui/Gui.js
@@ -1,10 +1,16 @@
 import * as dat from "dat.gui";
+import Stats from 'stats.js'
 
 export default class Gui {
 
     constructor() {
         this.gui = new dat.GUI();
         this.folders = new Map()
+
+        this.stats = new Stats()
+        this.stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+        document.body.appendChild(this.stats.dom)
+
     }
 
     addFolder(name) {
@@ -25,6 +31,14 @@ export default class Gui {
         if (this.folders.has(name)) {
             this.folders.get(name).add(object, property, start, end).listen()
         }
+    }
+
+    statsStart() {
+        this.stats.begin()
+    }
+
+    statsEnd() {
+        this.stats.end()
     }
 
 }
